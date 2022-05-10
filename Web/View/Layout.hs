6fb4c95951fecf2dc761cc88daade220d060b8bc
houzyk <houzairmk@icloud.com>
2022-05-10 15:31:49 +0400

    UPDATE layout

--- Web/View/Layout.hs
+++ Web/View/Layout.hs
@@ -12,54 +12,50 @@ import Application.Helper.View
 
 defaultLayout :: Html -> Html
 defaultLayout inner = H.docTypeHtml ! A.lang "en" $ [hsx|
-<head>
-    {metaTags}
-
-    {stylesheets}
-    {scripts}
-
-    <title>{pageTitleOrDefault "IHP BoilerPlate"}</title>
-</head>
-<body>
-    <div>
-        {renderFlashMessages}
-        {inner}
-    </div>
-</body>
+	<head>
+		{metaTags}
+		{stylesheets}
+		{scripts}
+		<title>{pageTitleOrDefault "IHP BoilerPlate"}</title>
+	</head>
+	<body>
+		{renderFlashMessages}
+		{inner}
+	</body>
 |]
 
 stylesheets :: Html
 stylesheets = [hsx|
-        <link rel="stylesheet" href={assetPath "/vendor/bootstrap.min.css"}/>
-        <link rel="stylesheet" href={assetPath "/vendor/flatpickr.min.css"}/>
-        <link rel="stylesheet" href={assetPath "/app.css"}/>
-    |]
+	<link rel="stylesheet" href={assetPath "/vendor/bootstrap.min.css"}/>
+	<link rel="stylesheet" href={assetPath "/vendor/flatpickr.min.css"}/>
+	<link rel="stylesheet" href={assetPath "/app.css"}/>
+|]
 
 scripts :: Html
 scripts = [hsx|
-        {when isDevelopment devScripts}
-        <script src={assetPath "/vendor/jquery-3.6.0.slim.min.js"}></script>
-        <script src={assetPath "/vendor/timeago.js"}></script>
-        <script src={assetPath "/vendor/popper.min.js"}></script>
-        <script src={assetPath "/vendor/bootstrap.min.js"}></script>
-        <script src={assetPath "/vendor/flatpickr.js"}></script>
-        <script src={assetPath "/vendor/morphdom-umd.min.js"}></script>
-        <script src={assetPath "/vendor/turbolinks.js"}></script>
-        <script src={assetPath "/vendor/turbolinksInstantClick.js"}></script>
-        <script src={assetPath "/vendor/turbolinksMorphdom.js"}></script>
-        <script src={assetPath "/helpers.js"}></script>
-        <script src={assetPath "/ihp-auto-refresh.js"}></script>
-        <script src={assetPath "/app.js"}></script>
-    |]
+	{when isDevelopment devScripts}
+	<script src={assetPath "/vendor/jquery-3.6.0.slim.min.js"}></script>
+	<script src={assetPath "/vendor/timeago.js"}></script>
+	<script src={assetPath "/vendor/popper.min.js"}></script>
+	<script src={assetPath "/vendor/bootstrap.min.js"}></script>
+	<script src={assetPath "/vendor/flatpickr.js"}></script>
+	<script src={assetPath "/vendor/morphdom-umd.min.js"}></script>
+	<script src={assetPath "/vendor/turbolinks.js"}></script>
+	<script src={assetPath "/vendor/turbolinksInstantClick.js"}></script>
+	<script src={assetPath "/vendor/turbolinksMorphdom.js"}></script>
+	<script src={assetPath "/helpers.js"}></script>
+	<script src={assetPath "/ihp-auto-refresh.js"}></script>
+	<script src={assetPath "/app.js"}></script>
+|]
 
 devScripts :: Html
 devScripts = [hsx|
-        <script id="livereload-script" src={assetPath "/livereload.js"} data-ws={liveReloadWebsocketUrl}></script>
-    |]
+	<script id="livereload-script" src={assetPath "/livereload.js"} data-ws={liveReloadWebsocketUrl}></script>
+|]
 
 metaTags :: Html
 metaTags = [hsx|
-    <meta charset="utf-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-    {autoRefreshMeta}
+	<meta charset="utf-8"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+	{autoRefreshMeta}
 |]
