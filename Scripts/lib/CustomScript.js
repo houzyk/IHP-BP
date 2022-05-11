a4f30c479aed9d25fb9deccced39f0b3bf30ea0b
houzyk <houzairmk@icloud.com>
2022-05-11 22:26:41 +0400

    ADD scripts structure

---
+++ Scripts/lib/CustomScript.js
@@ -0,0 +1,11 @@
+const PATH = require('path');
+const FS = require('fs');
+const SHELL = require('shelljs');
+const TEXT_COLOR = require("colors/safe");
+const PROMPT = require('prompt');
+
+console.log("Custom Script Running...");
+
+// CLI Custom Script Goes Here
+
+console.log("Custom Script Ended.");
