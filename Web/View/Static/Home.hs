13704634b6104af9af4d3c7f5ffb5445baa712c5
houzyk <houzairmk@icloud.com>
2022-05-11 21:34:10 +0400

    ADD Component structure

--- Web/View/Static/Home.hs
+++ Web/View/Static/Home.hs
@@ -1,9 +1,13 @@
 module Web.View.Static.Home where
 import Web.View.Prelude
 
+-- Components Imports
+import Web.View.Components.ExampleComponentFolder.ExampleComponent
+
 data HomeView = HomeView
 
 instance View HomeView where
 	html HomeView = [hsx|
 		<!-- HTML goes here -->
+		{exampleComponent}
 |]
