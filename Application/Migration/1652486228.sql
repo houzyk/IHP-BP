55462abc011eeca06c6c7a463e7e2d5559b1491d
houzyk <houzairmk@icloud.com>
2022-05-14 03:57:41 +0400

    ADD example model

--- Application/Migration/1652486228.sql
+++ Application/Migration/1652486228.sql
@@ -3,12 +3,11 @@ BEGIN
     NEW.updated_at = NOW();
     RETURN NEW;
 END;
-$$ language plpgsql;
--- Your database schema. Use the Schema Designer at http://localhost:8001/ to add some tables.
+$$ language PLPGSQL;
 CREATE TABLE examples (
     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
-    updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL
 );
 CREATE INDEX examples_created_at_index ON examples (created_at);
 CREATE TRIGGER update_examples_updated_at BEFORE UPDATE ON examples FOR EACH ROW EXECUTE FUNCTION set_updated_at_to_now();
