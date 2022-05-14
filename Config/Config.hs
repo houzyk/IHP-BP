89be007d401eebbf31502887c1bfdf9beca1eddc
houzyk <houzairmk@icloud.com>
2022-05-15 01:35:31 +0400

    Revert "ADD tailwind framework"
    This reverts commit 045990e786c1753094f0c1bb319802a73ce39b87.

--- Config/Config.hs
+++ Config/Config.hs
@@ -3,10 +3,8 @@ module Config where
 import IHP.Prelude
 import IHP.Environment
 import IHP.FrameworkConfig
-import Web.View.CustomCSSFramework
 
 config :: ConfigBuilder
 config = do
     option Development
     option (AppHostname "localhost")
-    option customTailwind
