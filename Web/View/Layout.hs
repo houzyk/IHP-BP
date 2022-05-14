d4c9d8ea6b1131f040b07410411a8fe026bea6f6
houzyk <houzairmk@icloud.com>
2022-05-15 01:22:16 +0400

    DELETE jquery, bootsrap, flatpickr, popper

--- Web/View/Layout.hs
+++ Web/View/Layout.hs
@@ -26,19 +26,13 @@ defaultLayout inner = H.docTypeHtml ! A.lang "en" $ [hsx|
 
 stylesheets :: Html
 stylesheets = [hsx|
-	<link rel="stylesheet" href={assetPath "/vendor/bootstrap.min.css"}/>
-	<link rel="stylesheet" href={assetPath "/vendor/flatpickr.min.css"}/>
 	<link rel="stylesheet" href={assetPath "/app.css"}/>
 |]
 
 scripts :: Html
 scripts = [hsx|
 	{when isDevelopment devScripts}
-	<script src={assetPath "/vendor/jquery-3.6.0.slim.min.js"}></script>
 	<script src={assetPath "/vendor/timeago.js"}></script>
-	<script src={assetPath "/vendor/popper.min.js"}></script>
-	<script src={assetPath "/vendor/bootstrap.min.js"}></script>
-	<script src={assetPath "/vendor/flatpickr.js"}></script>
 	<script src={assetPath "/vendor/morphdom-umd.min.js"}></script>
 	<script src={assetPath "/vendor/turbolinks.js"}></script>
 	<script src={assetPath "/vendor/turbolinksInstantClick.js"}></script>
